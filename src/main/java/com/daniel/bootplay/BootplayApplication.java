eeaf3a5602fbf507808b437edb7d789576ce8c9f
dipengfei <dipengfei1982@gmail.com>
2017-05-16 00:16:04 +0800

    add data init runner

--- src/main/java/com/daniel/bootplay/BootplayApplication.java
+++ src/main/java/com/daniel/bootplay/BootplayApplication.java
@@ -1,12 +1,23 @@
 package com.daniel.bootplay;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootplayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootplayApplication.class, args);
-	}
+
+    @Bean
+    public CommandLineRunner dataInitRunner() {
+
+        return args -> {
+
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootplayApplication.class, args);
+    }
 }
