a0d124df6147b61fd102542d6b882eafd027feeb
dipengfei <dipengfei1982@gmail.com>
2017-05-15 23:34:45 +0800

    initial spring boot project created from start.spring.io

---
+++ src/main/java/com/daniel/bootplay/BootplayApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.bootplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootplayApplication.class, args);
+	}
+}
