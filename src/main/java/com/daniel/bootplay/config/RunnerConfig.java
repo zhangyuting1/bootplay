3d3885ccfaff1d798ea8ba3de5ea3638fed358fd
dipengfei <dipengfei1982@gmail.com>
2017-05-16 09:13:56 +0800

    add runner config

---
+++ src/main/java/com/daniel/bootplay/config/RunnerConfig.java
@@ -0,0 +1,20 @@
+package com.daniel.bootplay.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by dipengfei on 16/05/2017.
+ */
+@Configuration
+public class RunnerConfig {
+
+    @Bean
+    public CommandLineRunner dataInitRunner() {
+
+        return args -> {
+
+        };
+    }
+}
